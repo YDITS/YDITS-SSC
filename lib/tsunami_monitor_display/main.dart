78a360d5148039f3cc686d5fcce069ece9f4fb46
よね/Yone <git@yoneyo.com>
2024-11-25 12:24:56 +0900

    Style: Format codes

--- lib/tsunami_monitor_display/main.dart
+++ lib/tsunami_monitor_display/main.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ydits_ssc/tsunami_monitor_display/widgets/app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    const App(),
+  );
 }
--- lib/weather_earthquake_telop_display/main.dart
+++ lib/weather_earthquake_telop_display/main.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ydits_ssc/weather_earthquake_telop_display/widgets/app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    const App(),
+  );
 }
