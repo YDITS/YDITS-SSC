c65910127156c1a2529a0a896d4467411aa22d29
よね/Yone <git@yoneyo.com>
2024-12-06 00:26:21 +0900

    add(eew): Loggerとwindow_sizeの追加

--- lib/weather_earthquake_telop_display/main.dart
+++ lib/weather_earthquake_telop_display/main.dart
@@ -8,9 +8,9 @@
 
 import 'dart:io';
 import 'package:logging/logging.dart';
+import 'package:window_size/window_size.dart';
 import 'package:flutter/material.dart';
 import 'package:ydits_ssc/weather_earthquake_telop_display/widgets/app.dart';
-import 'package:window_size/window_size.dart';
 import 'package:ydits_ssc/weather_earthquake_telop_display/config.dart';
 
 void main() async {
