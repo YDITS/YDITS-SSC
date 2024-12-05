96dbe7f6509223c7bbc9e00d9a2cfa432e5f3f51
よね/Yone <git@yoneyo.com>
2024-12-06 00:33:34 +0900

    change(eew): windowMaxSizeの変更

--- lib/eew_monitor_display/config.dart
+++ lib/eew_monitor_display/config.dart
@@ -10,7 +10,7 @@ import 'dart:ui';
 
 abstract final class Config {
   static String windowTitle = 'YDITS for SSC - EEW Monitor Display';
-  static Rect windowFrame = const Rect.fromLTWH(128, 128, 960, 128);
-  static Size windowMinSize = const Size(640, 128);
-  static Size windowMaxSize = const Size(1792, 128);
+  static Rect windowFrame = const Rect.fromLTWH(128, 128, 960, 540);
+  static Size windowMinSize = const Size(640, 360);
+  static Size windowMaxSize = Size.infinite;
 }
