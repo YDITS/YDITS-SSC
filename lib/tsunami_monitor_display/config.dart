e47d2a2e184746de67747064c24f46ceb59313ce
よね/Yone <git@yoneyo.com>
2024-12-06 00:38:59 +0900

    change(tsunami): `config.dart` window_size値の変更

--- lib/tsunami_monitor_display/config.dart
+++ lib/tsunami_monitor_display/config.dart
@@ -10,7 +10,7 @@ import 'dart:ui';
 
 abstract final class Config {
   static String windowTitle = 'YDITS for SSC - Tsunami Monitor Display';
-  static Rect windowFrame = const Rect.fromLTWH(128, 128, 960, 128);
-  static Size windowMinSize = const Size(640, 128);
+  static Rect windowFrame = const Rect.fromLTWH(128, 128, 960, 540);
+  static Size windowMinSize = const Size(640, 360);
   static Size windowMaxSize = Size.infinite;
 }
--- lib/weather_earthquake_telop_display/config.dart
+++ lib/weather_earthquake_telop_display/config.dart
@@ -12,5 +12,5 @@ abstract final class Config {
   static String windowTitle = 'YDITS for SSC - Weather Earthquake Telop Display';
   static Rect windowFrame = const Rect.fromLTWH(128, 128, 960, 128);
   static Size windowMinSize = const Size(640, 128);
-  static Size windowMaxSize = const Size(1792, 128);
+  static Size windowMaxSize = const Size(1920, 128);
 }
