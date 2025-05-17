b1697dc0a37b0063aa93e44d1372f77011a58e7f
よね/Yone <git@yoneyo.com>
2025-05-17 17:47:04 +0900

    Feat(version): Add Version class with versioning functionality

---
+++ lib/packages/version/version.dart
@@ -0,0 +1,38 @@
+//
+// Version
+//
+// Copyright (C) よね/Yone
+//
+
+class Version {
+    Version({
+        required this.major,
+        required this.minor,
+        required this.patch,
+        required this.level,
+    });
+
+    int major;
+    int minor;
+    int patch;
+    VersionLevels level;
+
+    static Map<VersionLevels, String> versionLevelTexts = {
+        VersionLevels.stable: "",
+        VersionLevels.beta: "Beta",
+        VersionLevels.develop: "Develop"
+    };
+
+    String get text {
+        String version = "${major}.${minor}.${patch}";
+        if (level == VersionLevels.stable) return version;
+        String levelText = versionLevelTexts[level] ?? "";
+        return "${version} (${levelText})";
+    }
+}
+
+enum VersionLevels {
+    stable,
+    beta,
+    develop,
+}
