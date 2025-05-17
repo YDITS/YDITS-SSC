0d3c04af8187ac345d89042019408a33ecde9cc0
よね/Yone <git@yoneyo.com>
2025-05-18 00:24:10 +0900

    Feat(Home): Update Home page contents

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -21,15 +21,62 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  final Map<Screens, String> screensTitle = {
+    Screens.eewMonitorDisplay: "EEW Monitor Display",
+    Screens.tsunamiMonitorDisplay: "Tsunami Monitor Display",
+    Screens.weatherEarthquakeTelopDisplay: "Weather Earthquake Telop Display",
+  };
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Text(widget.title),
+      body: Column(
+        children: [
+          Text(widget.title, style: const TextStyle(fontSize: 24)),
+          SizedBox(
+            height: 24,
+          ),
+          Expanded(
+            child: ListView.separated(
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return TextButton(
+                  onPressed: () => onScreenRootingButtonPress(index),
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    backgroundColor: Colors.blueGrey[800],
+                  ),
+                  child: SizedBox(
+                    height: 64,
+                    child: Center(
+                      child: Text(screensTitle.values.toList()[index],
+                          style: const TextStyle(color: Colors.white)),
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(height: 16),
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  void onScreenRootingButtonPress(index) {
+    final pressedScreen = Screens.values[index];
+    final pressedScreenTitle = screensTitle[pressedScreen];
+    print("Screen Rooting Button was clicked: Screen `$pressedScreen`, Screen Title `$pressedScreenTitle`");
+  }
+}
+
+enum Screens {
+  eewMonitorDisplay,
+  tsunamiMonitorDisplay,
+  weatherEarthquakeTelopDisplay,
 }
