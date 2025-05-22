2ef4a6378241e2df1a4c0c78337c672929a97d0c
よね/Yone <git@yoneyo.com>
2025-05-22 20:35:32 +0900

    Ref: Update application and window configurations for sub windows

--- lib/src/windows/tsunami_monitor_display/config.dart
+++ lib/src/windows/tsunami_monitor_display/config.dart
@@ -7,10 +7,41 @@
 //
 
 import 'dart:ui';
+import 'package:ydits_ssc/core/version/version.dart';
+import 'package:ydits_ssc/core/configure/app_config.dart';
+import 'package:ydits_ssc/core/configure/window_config.dart';
 
-abstract final class Config {
-  static String windowTitle = 'YDITS for SSC - Tsunami Monitor Display';
-  static Rect windowFrame = const Rect.fromLTWH(128, 128, 960, 540);
-  static Size windowMinSize = const Size(640, 360);
-  static Size windowMaxSize = Size.infinite;
+/// Tsunami Monitor Display アプリケーションの設定
+class TsunamiMonitorDisplayAppConfig implements AppConfig {
+  /// アプリケーションのタイトル
+  @override
+  final String title = "YDITS for SSC - Tsunami Monitor Display";
+
+  /// アプリケーションの説明
+  @override
+  final String description = "Saitama Sora Cam 用に制作された防災情報アプリケーション。";
+
+  /// アプリケーションのバージョン
+  @override
+  final Version version =
+      Version(major: 1, minor: 0, patch: 0, level: VersionLevels.develop);
+}
+
+/// Tsunami Monitor Display ウィンドウの設定
+class TsunamiMonitorDisplayWindowConfig implements WindowConfig {
+  /// ウィンドウのタイトル
+  @override
+  final String title = "YDITS for SSC - Tsunami Monitor Display";
+
+  /// ウィンドウのフレームデータ
+  @override
+  final Rect windowFrame = const Rect.fromLTWH(128, 128, 960, 540);
+
+  /// ウィンドウの初期サイズ
+  @override
+  final Size windowMinSize = const Size(640, 360);
+
+  /// ウィンドウの最大サイズの制限値
+  @override
+  final Size windowMaxSize = Size.infinite;
 }
--- lib/src/windows/weather_earthquake_telop_display/config.dart
+++ lib/src/windows/weather_earthquake_telop_display/config.dart
@@ -7,18 +7,64 @@
 //
 
 import 'dart:ui';
+import 'package:ydits_ssc/core/version/version.dart';
+import 'package:ydits_ssc/core/configure/app_config.dart';
+import 'package:ydits_ssc/core/configure/window_config.dart';
 
-abstract final class Config {
-  static String windowTitle =
-      'YDITS for SSC - Weather Earthquake Telop Display';
-  static Rect windowFrame = const Rect.fromLTWH(128, 128, 960, 128);
-  static Size windowMinSize = const Size(640, 128);
-  static Size windowMaxSize = const Size(1920, 128);
+abstract final class WeatherEarthquakeTelopDisplayConfig {
+  /// デフォルトのフォントサイズ
   static double fontSize = 32;
+
+  /// フォント
   static String fontFamily = "M PLUS 2";
+
+  /// テロップのラベルの幅
   static double labelWidth = 144;
+
+  /// テロップのスライドアニメーション速度
   static double telopSpeed = 100;
+
+  /// テロップのラベルの初期値
   static String initialLabelText = "起動中…";
+
+  /// テロップのコンテントの初期値
   static String initialContentText = "情報を取得しています…";
+
+  /// Open Weather Map の API Key
   static String openWeatherMapApiKey = "c1aa80da02ee7ace26f8a124c82b831d";
+}
+
+/// Weather Earthquake Telop Display アプリケーションの設定
+class WeatherEarthquakeTelopDisplayAppConfig implements AppConfig {
+  /// アプリケーションのタイトル
+  @override
+  final String title = "YDITS for SSC - Weather Earthquake Telop Display";
+
+  /// アプリケーションの説明
+  @override
+  final String description = "Saitama Sora Cam 用に制作された防災情報アプリケーション。";
+
+  /// アプリケーションのバージョン
+  @override
+  final Version version =
+      Version(major: 1, minor: 0, patch: 0, level: VersionLevels.develop);
+}
+
+/// Tsunami Monitor Display ウィンドウの設定
+class WeatherEarthquakeTelopDisplayWindowConfig implements WindowConfig {
+  /// ウィンドウのタイトル
+  @override
+  final String title = "YDITS for SSC - Weather Earthquake Telop Display";
+
+  /// ウィンドウのフレームデータ
+  @override
+  final Rect windowFrame = const Rect.fromLTWH(128, 128, 960, 128);
+
+  /// ウィンドウの初期サイズ
+  @override
+  final Size windowMinSize = const Size(640, 128);
+
+  /// ウィンドウの最大サイズの制限値
+  @override
+  final Size windowMaxSize = const Size(1920, 128);
 }
