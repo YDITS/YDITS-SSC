2745993a72882b0e5aa60729cb57ac73136f15a1
よね/Yone <git@yoneyo.com>
2025-06-01 22:26:58 +0900

    Feat(HomePageState): Update home page UI

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -12,11 +12,7 @@ import 'package:ydits_ssc/core/sub_windows/sub_windows.dart';
 import 'package:ydits_ssc/core/sub_windows/sub_windows_title.dart';
 
 final class HomePage extends StatefulWidget {
-  const HomePage({
-    super.key,
-    required this.title,
-    required this.windows,
-  });
+  const HomePage({super.key, required this.title, required this.windows});
 
   final String title;
   final Map<SubWindows, WindowController> windows;
@@ -30,39 +26,37 @@ final class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          style: const TextStyle(color: Colors.white70),
+        ),
+        backgroundColor: Colors.black87,
       ),
       body: Column(
         children: [
-          Text(widget.title, style: const TextStyle(fontSize: 24)),
-          const SizedBox(
-            height: 24,
+          const SizedBox(width: null, height: 16 * 2),
+          Text(
+            widget.title,
+            style: const TextStyle(fontSize: 16 * 2, color: Colors.black87),
           ),
+          const SizedBox(width: null, height: 16 * 2),
           Expanded(
-            child: ListView.separated(
-              itemCount: 3,
-              itemBuilder: (context, index) {
-                final pressedScreen = SubWindows.values[index];
-
-                return TextButton(
-                  onPressed: () =>
-                      _onSubWindowsRootingButtonPressed(pressedScreen),
-                  style: TextButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    backgroundColor: Colors.blueGrey[800],
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+              child: Center(
+                child: SizedBox(
+                  width: 769,
+                  height: null,
+                  child: ListView.separated(
+                    itemCount: 3,
+                    itemBuilder:
+                        (context, index) => windowsRootingButton(index),
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(width: null, height: 16 * 1.5);
+                    },
                   ),
-                  child: SizedBox(
-                    height: 64,
-                    child: Center(
-                      child: Text(subWindowsTitle.values.toList()[index],
-                          style: const TextStyle(color: Colors.white)),
-                    ),
-                  ),
-                );
-              },
-              separatorBuilder: (context, index) => const SizedBox(height: 16),
+                ),
+              ),
             ),
           ),
         ],
@@ -70,8 +64,30 @@ final class HomePageState extends State<HomePage> {
     );
   }
 
+  TextButton windowsRootingButton(index) {
+    final pressedScreen = SubWindows.values[index];
+
+    return TextButton(
+      onPressed: () => _onSubWindowsRootingButtonPressed(pressedScreen),
+      style: TextButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        backgroundColor: Colors.blueGrey[800],
+      ),
+      child: SizedBox(
+        height: 16 * 4,
+        child: Center(
+          child: Text(
+            subWindowsTitle.values.toList()[index],
+            style: const TextStyle(fontSize: 16, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
   Future<void> _onSubWindowsRootingButtonPressed(
-      SubWindows pressedScreen) async {
+    SubWindows pressedScreen,
+  ) async {
     print(widget.windows);
     print("Sub windows rooting button has clicked: `$pressedScreen`.");
 
