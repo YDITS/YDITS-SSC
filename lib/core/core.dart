570829c837ebefb7b475a421491589d781e64c0c
よね/Yone <git@yoneyo.com>
2025-06-03 14:27:25 +0900

    Ref(core): Add core.dart file with exports for core components

---
+++ lib/core/core.dart
@@ -0,0 +1,14 @@
+//
+// YDITS for SSC
+//
+// Copyright (C) よね/Yone
+//
+// https://github.com/YDITS/YDITS-SSC
+//
+
+export 'package:ydits_ssc/core/components/components.dart';
+export 'package:ydits_ssc/core/consts/consts.dart';
+export 'package:ydits_ssc/core/map/map.dart';
+export 'package:ydits_ssc/core/resources/resources.dart';
+export 'package:ydits_ssc/core/sub_windows/sub_windows_enum.dart';
+export 'package:ydits_ssc/core/window_manager/window_manager.dart';
