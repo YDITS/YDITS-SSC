ea90d57c219a0e5263141db09a6e92468b248614
よね/Yone <git@yoneyo.com>
2025-06-03 15:02:01 +0900

    Fix(consts): Update copyright_name export

--- lib/core/consts/consts.dart
+++ lib/core/consts/consts.dart
@@ -6,4 +6,5 @@
 // https://github.com/YDITS/YDITS-SSC
 //
 
-export 'package:ydits_ssc/core/consts/copyright_name/copyright_name.dart';
+export 'package:ydits_ssc/core/consts/copyright_name/copyright_name_provider.dart';
+export 'package:ydits_ssc/core/consts/flutter_map/flutter_map_consts.dart';
