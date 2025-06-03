49058fd970d4c3f50bc8cd656b4c978ac6098204
よね/Yone <git@yoneyo.com>
2025-06-03 15:04:02 +0900

    Fix(export): Update export statement for sub_windows to correct path

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -10,5 +10,5 @@ export 'package:ydits_ssc/core/components/components.dart';
 export 'package:ydits_ssc/core/consts/consts.dart';
 export 'package:ydits_ssc/core/map/map.dart';
 export 'package:ydits_ssc/core/resources/resources.dart';
-export 'package:ydits_ssc/core/sub_windows/sub_windows_enum.dart';
+export 'package:ydits_ssc/core/sub_windows/sub_windows.dart';
 export 'package:ydits_ssc/core/window_manager/window_manager.dart';
