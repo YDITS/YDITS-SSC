700180089d5c214bb3d5a89856e58737c36d43b3
よね/Yone <git@yoneyo.com>
2025-06-03 14:28:02 +0900

    Ref(components): Create components.dart file to export core components

---
+++ lib/core/components/components.dart
@@ -0,0 +1,10 @@
+//
+// YDITS for SSC
+//
+// Copyright (C) よね/Yone
+//
+// https://github.com/YDITS/YDITS-SSC
+//
+
+export 'package:ydits_ssc/core/components/copyright_footer/copyright_footer.dart';
+export 'package:ydits_ssc/core/components/icon_with_text_button/icon_with_text_button.dart';
