fc01cfbb8ebd38c3b86d92b008e2bbdedc11cb3b
よね/Yone <git@yoneyo.com>
2025-06-03 14:30:15 +0900

    Refactor(sub_windows): Separate SubWindows enum into its own file and update imports

--- lib/core/sub_windows/sub_windows.dart
+++ lib/core/sub_windows/sub_windows.dart
@@ -6,8 +6,6 @@
 // https://github.com/YDITS/YDITS-SSC
 //
 
-enum SubWindows {
-  eewMonitorDisplay,
-  tsunamiMonitorDisplay,
-  weatherEarthquakeTelopDisplay,
-}
+export 'package:ydits_ssc/core/sub_windows/sub_windows_enum.dart';
+export 'package:ydits_ssc/core/sub_windows/sub_windows_from_string.dart';
+export 'package:ydits_ssc/core/sub_windows/sub_windows_title.dart';
