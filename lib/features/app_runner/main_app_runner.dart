e38c761862f3de7f64759bb21d60282f9b753ee5
よね/Yone <git@yoneyo.com>
2025-06-08 13:42:49 +0900

    Refactor(YditsSscMainAppRunner): Improve logging and streamline app initialization

--- lib/features/app_runner/main_app_runner.dart
+++ lib/features/app_runner/main_app_runner.dart
@@ -11,35 +11,32 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import "package:configure/configure.dart";
 
 import 'package:ydits_ssc/core/app_runner/main_app_runner.dart';
-import 'package:ydits_ssc/core/window_manager/window_manager.dart';
 import 'package:ydits_ssc/features/main_app/main_app.dart';
-import 'package:ydits_ssc/features/main_app/configure/main_app_config.dart';
 import 'package:ydits_ssc/features/main_app/configure/main_app_window_config.dart';
 
 /// YDITS for SSC メインアプリケーションの実行管理
 final class YditsSscMainAppRunner extends MainAppRunner {
   YditsSscMainAppRunner({super.logger});
 
+  /// ウィンドウの構成
   @override
   final WindowConfig windowConfig = YditsSscWindowConfig();
 
   /// アプリケーションを実行する
   @override
   Future<void> run() async {
-    logger?.info("Starting Main application...");
-    WidgetsFlutterBinding.ensureInitialized();
-  
-    final subWindows = await configureSubWindows();
+    logger?.info("Running main application...");
 
+    WidgetsFlutterBinding.ensureInitialized();
+    final subWindows = await configureSubWindows();
     await initializeDesktopWindow();
+
+    app = YditsSscApp(subWindows: subWindows);
 
     runApp(
       ProviderScope(
-        child: YditsSscApp(config: YditsSscAppConfig(), subWindows: subWindows),
+        child: app,
       ),
     );
   }
-
-  /// WindowManagerインスタンス
-  late final YditsSscWindowManager subWindowManager;
 }
