f5ac527443e893b155f107debb62623e001f35aa
よね/Yone <git@yoneyo.com>
2025-06-08 13:41:47 +0900

    Feat(util): Add utility to check if the platform is desktop

---
+++ lib/core/utils/is_platform_desktop.dart
@@ -0,0 +1,11 @@
+//
+// YDITS for SSC
+//
+// Copyright (C) よね/Yone
+//
+// https://github.com/YDITS/YDITS-SSC
+//
+
+import 'dart:io';
+
+bool isPlatformDesktop = Platform.isLinux || Platform.isMacOS || Platform.isWindows;
