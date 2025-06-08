6135fb5e59df40ce6c97912d33704bcf7ba9d7a7
よね/Yone <git@yoneyo.com>
2025-06-08 13:37:18 +0900

    Ref(main): Improve logger initialization and error handling in main function

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,31 @@
 //
 
 import 'package:logging/logging.dart';
+import 'package:ydits_ssc/core/exceptions/exceptions.dart';
 import 'package:ydits_ssc/features/app_runner/app_runner.dart';
 
-const String loggerName = "ydits_ssc";
-const Level loggerRootLevel = Level.ALL;
-late final Logger logger;
-
 Future<void> main(List<String> args) async {
-  _initializeLogger();
-  await YditsSscAppRunner(args: args, logger: logger).runApp();
+  final Logger? logger = _initializeLogger();
+
+  try {
+    await YditsSscAppRunner(args: args, logger: logger).runApp();
+  } catch (error) {
+    print(error);
+  }
 }
 
 /// Loggerをイニシャライズする
-void _initializeLogger() {
-  logger = Logger(loggerName);
-  Logger.root.level = loggerRootLevel;
-  Logger.root.onRecord.listen((record) {
-    print('${record.level.name}: ${record.time}: ${record.message}');
-  });
+Logger? _initializeLogger() {
+  try {
+    final Logger logger = Logger("ydits_ssc");
+
+    Logger.root.level = Level.ALL;
+    Logger.root.onRecord.listen((record) {
+      print('${record.level.name}: ${record.time}: ${record.message}');
+    });
+    return logger;
+  } catch (error) {
+    print(LoggerInitializationFailed(error).toString());
+    return null;
+  }
 }
