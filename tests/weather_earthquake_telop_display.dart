d1e1d683b530c9f23c00a169348dd4907a6dfd56
よね/Yone <git@yoneyo.com>
2025-07-02 00:07:38 +0900

    Test(WeatherEarthquakeTelopDisplayEntry): Create direct entry dart file for testing its application

---
+++ tests/weather_earthquake_telop_display.dart
@@ -0,0 +1,13 @@
+//
+// YDITS for SSC
+//
+// Copyright (C) よね/Yone
+//
+// https://github.com/YDITS/YDITS-SSC
+//
+
+import 'package:ydits_ssc/apps/weather_earthquake_telop_display/weather_earthquake_telop_display.dart';
+
+Future<void> main(List<String> args) async {
+  WeatherEarthquakeTelopDisplay().main();
+}
