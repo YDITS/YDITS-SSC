d5b841b7cd3bd25e2943507654048f9bd827040f
よね/Yone <git@yoneyo.com>
2025-07-08 22:29:43 +0900

    Refactor(SubAppRunner): update documentation for clarity and consistency

--- lib/core/app_runner/sub_app_runner.dart
+++ lib/core/app_runner/sub_app_runner.dart
@@ -8,13 +8,19 @@
 
 import 'package:logging/logging.dart';
 
-/// サブアプリケーションの実行管理
+/// An abstract class for managing the execution of sub-applications.
+///
+/// Subclasses must implement the [run] method to define how a sub-application
+/// is launched based on the provided arguments.
 abstract class SubAppRunner {
   SubAppRunner({this.logger});
 
-  /// Logger インスタンス
+  /// An optional logger instance for logging application events.
   final Logger? logger;
 
-  /// サブアプリケーションを実行する
+  /// Runs a sub-application based on the given [args].
+  ///
+  /// This method is responsible for interpreting the arguments and launching the
+  /// corresponding sub-application.
   Future<void> run(List<String> args);
 }
