1ffd2d799368afb802966f12989b0b553a2ed8e6
よね/Yone <git@yoneyo.com>
2025-07-08 17:09:20 +0900

    Feature: create flutter_secure_storage_provider.dart and its part code

---
+++ lib/core/providers/flutter_secure_storage/flutter_secure_storage_provider.dart
@@ -0,0 +1,16 @@
+//
+// YDITS for SSC
+//
+// Copyright (C) よね/Yone
+//
+// https://github.com/YDITS/YDITS-SSC
+//
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'flutter_secure_storage_provider.g.dart';
+
+@riverpod
+FlutterSecureStorage storage(Ref ref) => const FlutterSecureStorage();
---
+++ lib/core/providers/flutter_secure_storage/flutter_secure_storage_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'flutter_secure_storage_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$storageHash() => r'444b45682708efa93ecb645d3b4190f0d84d5c57';
+
+/// See also [storage].
+@ProviderFor(storage)
+final storageProvider = AutoDisposeProvider<FlutterSecureStorage>.internal(
+  storage,
+  name: r'storageProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$storageHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef StorageRef = AutoDisposeProviderRef<FlutterSecureStorage>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
