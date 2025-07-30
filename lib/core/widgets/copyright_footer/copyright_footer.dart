cd6e517767b939798e5c4e6074df81f889d0fcc6
よね/Yone <git@yoneyo.com>
2025-07-30 13:45:30 +0900

    Refactor(CopyrightFooter): Use theme provider for background color
    This commit updates the `CopyrightFooter` widget to use the newly introduced `themeProvider` for setting its background color.
    
    Previously, the background color was hard-coded as `Colors.black87`. This has been replaced with `theme.darkBackground` from the `ThemeModel` to ensure that the footer's appearance is consistent with the overall application theme.
    
    This change improves theme adherence and makes the widget more maintainable and scalable.

--- lib/core/widgets/copyright_footer/copyright_footer.dart
+++ lib/core/widgets/copyright_footer/copyright_footer.dart
@@ -9,6 +9,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ydits_ssc/core/providers/constants/copyright_name/copyright_name_provider.dart';
+import 'package:ydits_ssc/core/providers/theme/model/theme_model.dart';
+import 'package:ydits_ssc/core/providers/theme/notifier/theme_notifier.dart';
 
 /// A widget that displays a copyright footer.
 class CopyrightFooter extends ConsumerWidget {
@@ -17,9 +19,10 @@ class CopyrightFooter extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final String name = ref.watch(copyrightNameProvider);
+    final ThemeModel theme = ref.watch(themeProvider);
 
     return Container(
-      color: Colors.black87,
+      color: theme.darkBackground,
       padding: const EdgeInsets.all(12),
       child: Center(
         child: Text(
